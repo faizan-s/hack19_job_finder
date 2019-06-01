97fcf3ed9fb0e081222466c6e6f4c9b0e96b76e2
Rohit Kawade <rohit.kawade@globant.com>
2019-06-01 11:29:47 +0530

    created a home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new HomePageBody(),
+    );
+  }
+}
+
+class HomePageBody extends StatefulWidget {
+  @override
+  _HomePageBodyState createState() => new _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<HomePageBody> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Text("Job Finder"),
+      ],
+    );
+  }
+}
