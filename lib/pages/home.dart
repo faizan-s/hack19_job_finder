c3b8b5b7c759a42610bb4896a63a3e02538501f4
akashbhingole <akash.bhingole@globant.com>
2019-06-01 12:37:18 +0530

    Added app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: getAppBar(),
       body: new HomePageBody(),
     );
   }
@@ -27,4 +27,37 @@ class _HomePageBodyState extends State<HomePageBody> {
       ],
     );
   }
+}
+
+getAppBar() {
+  return AppBar(
+    backgroundColor: Colors.blue,
+      actions: <Widget>[
+        IconButton(
+          icon: new Icon(Icons.menu), 
+          onPressed: (){}
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 10.0),
+          child: Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white, //remove this when you add image.
+            ),
+          ),
+        ), 
+        IconButton(icon: new Icon(Icons.notifications), onPressed: (){}),
+      ],
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+      textTheme: TextTheme(
+        title: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        )
+      ),
+  )  ; 
 }
