432760ef0e98eaea6d76a898987c73dfd4f06c71
akashbhingole <akash.bhingole@globant.com>
2019-06-01 15:29:32 +0530

    Changed images

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -37,30 +37,26 @@ getAppBar() {
   return AppBar(
     backgroundColor: Colors.white,
     iconTheme: new IconThemeData(color: Colors.blue),
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Container(
-            alignment: Alignment.center,
-            width: 50.0,
-            height: 50.0,
-            decoration: new BoxDecoration(
-              shape: BoxShape.circle,
-              image: new DecorationImage(
-                  fit: BoxFit.fill,
-                  image: new NetworkImage(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8fhxpPX98Y2vcd6P230As-0WWa8BPrhRu8TL3rZeFZyUZT_ox",
-                  )
-              )
-          )),
-        IconButton(
-          alignment: Alignment.centerRight,
-          icon: new Icon(Icons.notifications), 
-          onPressed: (){}
+    title: Text('Job Search'),
+    actions : <Widget>[
+      Container(
+        alignment: Alignment.centerLeft,
+        width: 50.0,
+        decoration: new BoxDecoration(
+          shape: BoxShape.circle,
+          image: new DecorationImage(
+            image: new NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8fhxpPX98Y2vcd6P230As-0WWa8BPrhRu8TL3rZeFZyUZT_ox",
+            )
+          )
         )
-      ],
-    )  
+      ),
+      IconButton(
+        alignment: Alignment.centerRight,
+        icon: new Icon(Icons.notifications), 
+        onPressed: (){}
+      )
+    ],  
   ); 
 }
 
